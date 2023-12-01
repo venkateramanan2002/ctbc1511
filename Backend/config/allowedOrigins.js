bdb3901eab2cd56adc48476143690090eba1f7d0
Venkate Ramanan <72510646+venkateramanan2002@users.noreply.github.com>
2023-12-01 17:22:10 +0530

    Update allowedOrigins.js

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:5173","http://localhost:5174","http://ctbci.org"];
+const allowedOrigins = ["http://localhost:5173","http://localhost:5174","http://ctbci.org","https://ctbci.org"];
 
 module.exports = allowedOrigins;
